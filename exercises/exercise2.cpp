e43d375080968fc2b0bddec6b4006831e4dd149d
Katie Barrett <kayt.barrett22@gmail.com>
2024-06-12 14:47:52 -0600

    Exercise 5 complete as extension of exercise 2

--- exercises/exercise2.cpp
+++ exercises/exercise2.cpp
@@ -3,6 +3,7 @@
     This code has many errors in it. Find the errors, list them (Ill add a comment) and correct it.
     Exercise 3: Place a check on the program that will produce an error if a temp given is below -273.15K.
     Exercise 4: Do exercise 3 again, but this time handle the error inside ctok().
+    Exercise 5: Add to the program so that it can also convert from Kelvin to Celsius.
 */
 
 #include <iostream>
@@ -19,11 +20,37 @@ double ctok(double c)
     return k;
 }
 
-int main(){
-    double c = 0;
-    cout << "Please enter a temperature in Celsius: \n";
-    cin >> c;
+double ktoc(double k)
+{
+    if (k < 0){
+        cerr << "Error: Please enter a value greater than 0 degrees Kelvin (-273.15C).\n";
+        exit(EXIT_FAILURE);
+    }
+    double c = k - 273.15;
+    return c;
+}
 
-    double k = ctok(c);
-    cout << "This is " << k << " degrees Kelvin\n";
+int main(){
+    double temp = 0;
+    char unit;
+    cout << "Please enter a temperature followed by a unit (C, K). Please use a space after temperature (ex: 23 C): \n";
+    cin >> temp >> unit;
+
+    double convertedTemp;
+
+    switch(unit)
+    {
+        case 'C':
+            convertedTemp = ctok(temp);
+            cout << "This is " << convertedTemp << " degrees Kelvin\n";
+            break;
+        case 'K':
+            convertedTemp = ktoc(temp);
+            cout << "This is " << convertedTemp << " degrees Celsius\n";
+            break;
+        default:
+            cerr << "Error: Invalid input." << endl;
+            break;
+    }
+    
 }
