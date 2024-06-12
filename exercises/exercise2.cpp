d1f836cb86737bbf992ed7433e235b81305902c3
Katie Barrett <kayt.barrett22@gmail.com>
2024-06-12 14:21:44 -0600

    Exercise 4 complete, as an extension of exercise 2

--- exercises/exercise2.cpp
+++ exercises/exercise2.cpp
@@ -2,6 +2,7 @@
     Exercise 2: The following program takes ina  temperature value in Celsius and converts it to Kelvin.
     This code has many errors in it. Find the errors, list them (Ill add a comment) and correct it.
     Exercise 3: Place a check on the program that will produce an error if a temp given is below -273.15K.
+    Exercise 4: Do exercise 3 again, but this time handle the error inside ctok().
 */
 
 #include <iostream>
@@ -11,6 +12,10 @@ using namespace std;
 double ctok(double c)
 {
     double k = c + 273.15;
+    if (k < 0){
+        cerr << "Error: Please enter a value greater than 0 degrees Kelvin (-273.15C).\n";
+        exit(EXIT_FAILURE);
+    }
     return k;
 }
 
@@ -18,11 +23,6 @@ int main(){
     double c = 0;
     cout << "Please enter a temperature in Celsius: \n";
     cin >> c;
-    if (c < -273.15)
-    {
-        cerr << "Error: Please enter a value greater than 0 degrees Kelvin (-273.15C).";
-        return EXIT_FAILURE;
-    }
 
     double k = ctok(c);
     cout << "This is " << k << " degrees Kelvin\n";
